d5b24e3546d2c1f8ed26fa2dbfd110eb7731af1f
Bart Coppens <kde@bartcoppens.be>
2015-10-03 17:29:37 +0200

    Parse data files

--- qucsator.py
+++ qucsator.py
@@ -3,7 +3,8 @@ import subprocess
 
 import circuit
 
-# Hacky as hell
+# This entire file is hacky as hell! The way these files are 'parsed' is totally fragile. On the other hand, the 'formats' are not really well-defined to start with
+# This works for Qucs 0.0.19 (some git checkout)
 
 class Netlist:
     def parse(self, filename):
@@ -50,6 +51,57 @@ class Netlist:
         self.parse(filename) # sets circuit
 
 class Simulation:
+    def parse_datapoint(self, line):
+        # Horrible format
+        #   -3.3e+01
+        #   -1.2e-01-j8.4e-02
+
+        regex_re = "([+-]\d+.\d+e[+-]\d+)([+-]?)" # The last group matches the sign of the imaginary part, if any
+        regex_im = "(\d+.\d+e[+-]\d+)"
+        components = line.split("j")
+        m = re.search(regex_re, components[0])
+
+        r = float(m.group(1))
+        i = 0
+        if len(components) > 1:
+            negative = (m.group(2) == '-')
+            m = re.search(regex_im, components[1])
+            i = float(m.group(1))
+            if negative:
+                i = -i
+            return complex(r, i)
+        return r
+
+    def read_from_datafile(self, datafile):
+        # TODO more rigorous testing with more complext netlist files (multiple simulations)
+        self.data = {}
+        self.depends = {}
+
+        current_datalist = None
+        current_name = None
+
+        for line in open(datafile, "r"):
+            if line.startswith("<indep "):
+                current_name = line.split(" ")[1] # TODO: verify that we read in as much data lines as claimed in [2] here?
+                current_datalist = []
+
+            elif line.startswith("</indep>") or line.startswith("</dep>"):
+                self.data[current_name] = current_datalist
+                print "%s -> %s" % (current_name, str(current_datalist))
+                current_datalist = None
+
+            elif line.startswith("<dep "):
+                regex = "<dep ([^ ]+) ([^>]+)>"
+                m = re.search(regex, line)
+                current_name = m.group(1)
+                dependent = m.group(2)
+
+                self.depends[current_name] = dependent
+                current_datalist = []
+
+            elif current_datalist is not None:
+                current_datalist.append(self.parse_datapoint(line))
+
     def simulate(self, netlist):
         netlist_base = self.config.get("DEFAULT", "netlist_tmp_base", vars={"netlist_tmp_base": "temp"})
         netlist_file_in = netlist_base + ".net"
@@ -57,6 +109,8 @@ class Simulation:
 
         netlist.output(netlist_file_in) # TODO mktemp?
         subprocess.check_call([self.qucsator, "-i", netlist_file_in, "-o", datafile])
+
+        self.read_from_datafile(datafile)
 
     def __init__(self, config):
         self.qucsator = config.get("DEFAULT", "qucsator", vars={"qucsator": "qucsator"})
