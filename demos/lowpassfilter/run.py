9728a3a2173582f7d8dacc9d4cbbb8668433233f
Bart Coppens <kde@bartcoppens.be>
2015-10-18 22:23:19 +0200

    Simple physical model for inductors

--- demos/lowpassfilter/run.py
+++ demos/lowpassfilter/run.py
@@ -45,6 +45,9 @@ for c in ["C1", "C3", "C5"]:
     # Use on purpose a more leaky capacitor, so the difference shows up in the graphs :)
     physical.model_capacitor(netlist_realisation, c, R_L = circuit.Value("10 k"), R_ESR = circuit.Value("0"), L_ESL = circuit.Value("0"))
 
+for l in ["L2", "L4"]:
+    physical.model_inductor(netlist_realisation, l, R_P = circuit.Value("100 M"), R_S = circuit.Value("0"), C_P = circuit.Value("0"))
+
 # TODO: look at the trick from the qucs test cases to wrap the simulation data into multi-dimensional arrays?
 
 def acceptable_circuit(sim):
--- src/physical.py
+++ src/physical.py
@@ -22,3 +22,23 @@ def model_capacitor(netlist, capacitor, R_L, R_ESR, L_ESL):
                         
     r_esr = netlist.circuit.next_internal_name_for("R_ESR")
     netlist.circuit.add(circuit.Resistor(r_esr, R_ESR, None, net_after_ind, net_end))
+
+# Replace an inductor in a netlist with set of passive components that model parallel losses R_P (such as core losses), self-capacitance C_P, and the conductive resitance R_S
+def model_inductor(netlist, inductor, R_P, R_S, C_P):
+    l = netlist.circuit.get_component(inductor)
+
+    net_begin = l.port1
+    net_end   = l.port2
+
+    net_after_orig = netlist.circuit.next_internal_name_for("after_orig")
+
+    l.port2 = net_after_orig
+
+    r_s = netlist.circuit.next_internal_name_for("R_S")
+    netlist.circuit.add(circuit.Resistor(r_s, R_S, None, net_after_orig, net_end))
+
+    c_p = netlist.circuit.next_internal_name_for("C_P")
+    netlist.circuit.add(circuit.Capacitor(c_p, C_P, None, net_begin, net_end))
+
+    r_p = netlist.circuit.next_internal_name_for("R_P")
+    netlist.circuit.add(circuit.Resistor(r_p, R_P, None, net_begin, net_end))
